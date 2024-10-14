b10f70b17e6480030b3dfded904e1a6a7d324c50
rkssek <134579215+rkssek@users.noreply.github.com>
2024-10-15 00:20:24 +0900

    add EPL

---
+++ commands/EPL.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+async function getEPLRankings() {
+    try {
+        const { data } = await axios.get('https://www.espn.com/soccer/standings/_/league/eng.1');
+        const $ = cheerio.load(data);
+
+        const teams = [];
+
+        $('tbody tr').each((index, element) => {
+            if (index < 10) { 
+                let rank = $(element).find('td:nth-child(1)').text().trim(); 
+                const teamName = $(element).find('span.hide-mobile').text().trim(); 
+
+                rank = rank.replace(/\D/g, ''); 
+
+                teams.push(`${rank}. ${teamName}`);
+            }
+        });
+
+        return teams;
+    } catch (error) {
+        console.error('Error fetching EPL rankings:', error);
+        return [];
+    }
+}
+
+module.exports = {
+    name: 'epl',
+    description: '현재 EPL 상위 10위 팀을 보여줍니다.',
+    async run(client, message, args) {
+        const rankings = await getEPLRankings();
+        if (rankings.length > 0) {
+            message.channel.send(`현재 EPL 순위:\n${rankings.join('\n')}`);
+        } else {
+            message.channel.send('EPL 순위를 가져오는 데 실패했습니다.');
+        }
+    },
+};
--- commands/command.js
+++ commands/command.js
@@ -5,6 +5,7 @@ module.exports = {
       message.channel.send('!금지어 추가 : 금지어를 추가합니다.');
       message.channel.send('!금지어 목록 : 금지어 목록을 보여줍니다.');
       message.channel.send('!금지어 삭제 : 금지어를 삭제합니다.');
+      message.channel.send('!EPL : 현재 EPL 상위 10팀을 보여줍니다.');
     }
   };
   
